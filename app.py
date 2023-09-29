48ef00a035ab2acb41a59bfe3b61ccaafd0abaeb
Jared <jared_glenn@yahoo.com>
2023-09-29 10:19:56 -0600

    add get_cupcake and add_cupcake routes

--- app.py
+++ app.py
@@ -26,3 +26,31 @@ def all_cupcakes():
     all_cupcakes = [cupcake.serialize() for cupcake in Cupcake.query.all()]
     
     return jsonify(cupcakes=all_cupcakes)
+
+
+@app.route('/api/cupcakes/<cupcake_id>', methods=["GET"])
+def get_cupcake(cupcake_id):
+    """Get the identified cupcake."""
+    
+    cupcake = (Cupcake.query.get_or_404(cupcake_id)).serialize()
+    
+    return jsonify(cupcake=cupcake)
+
+
+@app.route('/api/cupcakes', methods=["POST"])
+def create_cupcake():
+    """Create a new cupcake."""
+    
+    new_cupcake = Cupcake(flavor=request.json["flavor"], size=request.json["size"], 
+                          rating=request.json["rating"], image=request.json["image"])
+    
+    db.session.add(new_cupcake)
+    db.session.commit()
+    response_json = jsonify(cupcake=new_cupcake.serialize())
+    return (response_json, 201)
+
+@app.route('/api/cupcakes/<cupcake_id>', methods=["PATCH"])
+def update_cupcake(cupcake_id):
+    """Update the specified cupcake."""
+    
+    
